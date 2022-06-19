244538d683df926b113dd7173c516fb701accb87
Waithera Munderu <wmunderu@gmail.com>
2022-06-19 08:05:31 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const words = word.length;  
+   
+  for (let i = 0; i < words / 2; i++) {   
+    if (word[i] !== word[words - 1 - i]) {  
+      return false;  
+    }  
+  }  
+  return true;  
 }
+
+
 
 /* 
   Add your pseudocode here
@@ -8,6 +18,10 @@ function isPalindrome(word) {
 
 /*
   Add written explanation of your solution here
+  The solution is based on whether the word at index i, that is its letter equal 
+  to the word at index (word.length - 1 - i).
+  If the it is not equal, it is not a palindrome.
+  If it is, it is a palindrome
 */
 
 // You can run `node index.js` to view these console logs
